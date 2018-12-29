5610893039708587d5e44409553ba6d188bb1a57
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-12-29 13:11:29 +0100

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+class Header extends Component {
+
+    state = {
+        isOpen: false
+    };
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">{this.props.siteTitle}</NavbarBrand>
+                    <NavbarToggler onClick={ this.toggle }/>
+                    <Collapse isOpen={ this.state.isOpen } navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/about/">About</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/imprint/">Imprint</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                                    </DropdownItem>
+                                    <DropdownItem divider/>
+                                    <DropdownItem>
+                                        Reset
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/pages/imprint.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Imprint = () => (
+    <Layout>
+        <SEO title="Imprint" />
+        <h1>Imprint</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit est lacus vivamus, dis maecenas eu nullam pharetra curae penatibus in purus imperdiet suspendisse, cum magna nisl quam massa nostra molestie vehicula montes. Velit dapibus diam vivamus porttitor viverra tortor netus, placerat nec cum felis class pharetra, mauris eleifend sodales ullamcorper iaculis nisi. Suspendisse dis suscipit litora eros et per duis nostra, nec vel metus lobortis urna convallis mauris rhoncus consequat, est orci donec platea morbi nisl interdum.</p>
+
+            <p>Quis torquent aptent tempor pulvinar posuere lacinia nam ullamcorper maecenas tortor, conubia metus iaculis bibendum penatibus massa fermentum dictum morbi, leo ante sollicitudin platea felis velit hendrerit dis euismod. Felis sagittis rutrum laoreet mus sem vel vivamus nostra rhoncus vehicula, sapien natoque potenti tortor pulvinar convallis luctus penatibus malesuada interdum, porta volutpat aliquet nisl arcu eget curabitur sodales morbi. Torquent taciti tristique sociis mattis fringilla at sodales in, fusce nascetur euismod sagittis etiam augue eros dictumst, velit dis arcu pretium fermentum interdum porttitor.</p>
+
+    </Layout>
+)
+
+export default Imprint
